658ef9f0a7f0ff635e32ab192f6fb4d2f6ba705d
“Elizabeth <elizabethsduan@gmail.com>
2026-04-28 22:43:27 -0700

    high resolution parameter sweep run

---
+++ hyak/hyak.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#SBATCH --job-name=PETRI_snakemake
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=esd4@uw.edu
+
+#SBATCH --account=biology
+#SBATCH --partition=compute-hugemem
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=39
+#SBATCH --mem=120G
+#SBATCH --time=24:00:00
+
+#SBATCH --export=all
+#SBATCH --chdir=/mmfs1/gscratch/biology/kerrlab/ESD/PETRI
+
+# Load conda module
+module load conda
+
+# Initialize Conda for the batch shell
+eval "$(conda shell.bash hook)"
+
+# Load environment
+conda activate snakemake_host
+
+# Run pipeline
+snakemake --cores 39 --use-conda
